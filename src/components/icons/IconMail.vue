8231b2e1bd985deb1aee52828cc18a761130588c
Taechai <belmaroufchaimae@gmail.com>
2024-01-03 22:40:49 -0500

    Added header components

---
+++ src/components/icons/IconMail.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="27" height="28" viewBox="0 0 27 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.5 5.188H22.5C23.7375 5.188 24.75 6.19309 24.75 7.42153V20.8227C24.75 22.0511 23.7375 23.0562 22.5 23.0562H4.5C3.2625 23.0562 2.25 22.0511 2.25 20.8227V7.42153C2.25 6.19309 3.2625 5.188 4.5 5.188Z" fill="#E0E0E0"/>
+<path d="M24.75 7.42153L13.5 15.2389L2.25 7.42153" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+</template>
